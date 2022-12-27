9a3da90eccaedd9969e5da21d37edfa90de3470f
Blackflame6 <engineerfranklyn@gmail.com>
2022-12-27 10:26:51 -0600

    installed tailwind and postcss with plugins

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
+import './styles.css'
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <header className='text-red-800'>Hello World</header>
+    </div>
+  );
 }
 
 export default App;
