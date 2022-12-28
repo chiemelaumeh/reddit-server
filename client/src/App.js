1856a91fa0ea26e2e46b2a873d64ca105bd1fb00
Blackflame6 <engineerfranklyn@gmail.com>
2022-12-27 21:27:52 -0600

    created reddit image section

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import "./header.css";
-import "./belowheader.css"
-import "./components/Header"
+import "./belowheader.css";
+import "./components/Header";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-     <Header />
-     <section className="below-header" >
-     
-     </section>
-     
+      <Header />
+      <div className="below-header"></div>
+      <div className="sub-main">
+        <div className="com-div">
+          <img className="com-icon"
+            src="https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_kxcmzy9bt1381.jpg?width=256&format=pjpg&s=0a2e472f6fae0712fee4a3b5d44920fe35dbcdaa"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
