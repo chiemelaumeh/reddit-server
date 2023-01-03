9baca50d0d35e4713d46b55e2d38b7c460c93ff3
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-02 22:42:23 -0600

    added email sign up field

--- client/src/components/Authmodal.js
+++ client/src/components/Authmodal.js
@@ -3,15 +3,18 @@ import Input from "./Input";
 import { useState } from "react";
 
 const Authmodal = () => {
-
-     
-  
   const [modalType, setModalType] = useState("login");
   return (
     <div className="auth-page">
       <div className="auth-sub">
         {modalType === "login" && <h1>Login</h1>}
         {modalType === "register" && <h1>Register</h1>}
+        {modalType === "register" && (
+          <label>
+            <span>E-mail: </span>
+            <Input type="email" />
+          </label>
+        )}
         <label>
           <span>Username: </span>
           <Input type="text" />
@@ -21,15 +24,16 @@ const Authmodal = () => {
 
           <Input type="password" />
         </label>
-        <Headerbuttons>Login</Headerbuttons>
+        <Headerbuttons>
+          {modalType === "login" ? "Log in" : "Sign Up"}
+        </Headerbuttons>
         {modalType === "login" && (
-           <div className="login-state">
-           <p className="login-p-1">New to Reddit?</p>
-           <button className="btn" onClick={() => setModalType("register")}>
-             SIGN UP
-           </button>
-         </div>
-       
+          <div className="login-state">
+            <p className="login-p-1">New to Reddit?</p>
+            <button className="btn" onClick={() => setModalType("register")}>
+              SIGN UP
+            </button>
+          </div>
         )}
         {modalType === "register" && (
           <div className="login-state">
