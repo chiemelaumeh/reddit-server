32f8ba89a7b6403670d745f95f407fd80f209a1b
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-02 20:19:06 -0600

    created login and register state

--- client/src/components/Authmodal.js
+++ client/src/components/Authmodal.js
@@ -1,11 +1,20 @@
 import Headerbuttons from "./Headerbuttons";
 import Input from "./Input";
+import { useState } from "react";
 
 const Authmodal = () => {
+  const [modalType, setModalType] = useState("login");
   return (
     <div className="auth-page">
       <div className="auth-sub">
+        {modalType === "login" && (
+
         <h1>Login</h1>
+        )}
+        {modalType === "register" && (
+
+        <h1>Register</h1>
+        )}
         <label>
           <span>Username: </span>
           <Input type="text" />
