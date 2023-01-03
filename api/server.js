a8eb44038d0fcdda7ff8ded362ba576a5d160e4e
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-03 17:51:42 -0600

    fixed cors error

--- api/server.js
+++ api/server.js
@@ -6,18 +6,30 @@ import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
 import User from "./models/User.js";
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    // methods:["GET", "POST"],
+    credentials: true,
+  })
+);
+
 
 // app.use(cors());
 // // allowing cors
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+// // app.use((req, res, next) => {
+// //   res.header("Access-Control-Allow-Origin", "*");
+// //   res.header("Access-Control-Allow-Methods", "*");
+// //   res.header(
+// //     "Access-Control-Allow-Headers",
+// //     "Origin, X-Requested-With, Content-Type, Accept"
+// //   );
+// //   next();
+// // });
 
 mongoose.set("strictQuery", false);
 await mongoose.connect("mongodb://localhost:27017/reddit", {
@@ -28,16 +40,8 @@ await mongoose.connect("mongodb://localhost:27017/reddit", {
 const db = mongoose.connection;
 db.on("error", console.log);
 
-const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  })
-);
+
+
 
 app.get("/", (req, res) => {
   res.send("ok");
