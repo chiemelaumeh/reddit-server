c623d3a2358c339dd8aa88a099f5631c2773bc6b
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-05 17:28:28 -0600

    assigned buttons to show when user is logged in or not

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,12 +5,15 @@ import OutsideClickHandler from "react-outside-click-handler";
 import AuthModalContext from "../context/AuthModalContext";
 import ModalContext from "../context/ModalContext";
 import { CiUser } from "react-icons/ci";
+import { BsBell } from "react-icons/bs";
+import { BsChatDots } from "react-icons/bs";
+import { HiOutlinePlus } from "react-icons/hi";
 import logo from "../images/logo.png";
 import { CiSearch } from "react-icons/ci";
 import { BsChevronDown } from "react-icons/bs";
 import { SlLogin } from "react-icons/sl";
 import UserContext from "../context/UserContext";
-import avatar from "../images/avatar.jpg";
+import avatar from "../images/chess.webp";
 
 const Header = () => {
   const [userDropDownVisibilityClass, setUserDropDownVisibilityClass] =
@@ -50,31 +53,31 @@ const Header = () => {
             placeholder="Search Reddit"
           />
         </form>
-
-        {/* <button className="icon-btn">
-          <BsBell className="icon" />
-        </button>
-        <button className="icon-btn">
-          <BsChatDots className="icon" />
-        </button>
-        <button className="icon-btn">
-          <HiOutlinePlus className="icon" />
-        </button> */}
-
-        <div className="login-div">
-          <Headerbuttons onClick={handleLogin}>Log In </Headerbuttons>
-          <Headerbuttons onClick={handleSignUp}>Sign Up </Headerbuttons>
-        </div>
+        {user && (
+          <>
+            <button className="icon-btn">
+              <BsBell className="icon" />
+            </button>
+            <button className="icon-btn">
+              <BsChatDots className="icon" />
+            </button>
+            <button className="icon-btn">
+              <HiOutlinePlus className="icon" />
+            </button>
+          </>
+        )}
+        {!user && (
+          <div className="login-div">
+            <Headerbuttons onClick={handleLogin}>Log In </Headerbuttons>
+            <Headerbuttons onClick={handleSignUp}>Sign Up </Headerbuttons>
+          </div>
+        )}
 
         <OutsideClickHandler
           onOutsideClick={() => setUserDropDownVisibilityClass("hidden")}
         >
           <button className="avatar-btn" onClick={toggleDropDown}>
-            {!user.username && (
-
-              <CiUser className="icon" />
-            )}
-            
+            {!user.username && <CiUser className="icon" />}
 
             {user.username && (
               <>
