7bdb8a9be3c6c23d8dc119cf8b22d45699a4ef09
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 14:36:02 -0600

    updated cors

--- api/server.js
+++ api/server.js
@@ -17,14 +17,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "https://reddit-app-nw97.onrender.com",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 const secret = process.env.SECRET_KEY;
 const connectionString = process.env.DATABASE_URL
 
