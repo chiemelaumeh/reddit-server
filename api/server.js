4026724da0468c90ad571d7e80224a09086f6594
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 16:26:28 -0600

    updated cors

--- api/server.js
+++ api/server.js
@@ -17,30 +17,13 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-// app.use(cors());
-
-// // allowing cors
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://reddit-app-nw97.onrender.com"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 const secret = process.env.SECRET_KEY;
 const connectionString = process.env.DATABASE_URL
