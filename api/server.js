9f7cb35f649fdecdaee205f94156b0a53a24447f
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 15:45:56 -0600

    fixed cors

--- api/server.js
+++ api/server.js
@@ -17,19 +17,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://reddit-app-nw97.onrender.com",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-app.configure(function() {
-  app.use(allowCrossDomain);
- 
-}); 
 const secret = process.env.SECRET_KEY;
 const connectionString = process.env.DATABASE_URL
 
