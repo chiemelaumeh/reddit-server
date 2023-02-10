6e05a7cdc2847733a41f9658eb6b892f4992f879
Blackflame6 <engineerfranklyn@gmail.com>
2023-02-10 01:53:52 -0600

    added react markdown package

--- client/src/components/PostFormModal.js
+++ client/src/components/PostFormModal.js
@@ -3,6 +3,8 @@ import { useState, useContext } from "react";
 import AuthModalContext from "../context/AuthModalContext";
 import Input from "./Input";
 import TextArea from "./TextArea";
+import ReactMarkdown from "react-markdown"
+import gfm from "remark-gfm"
 
 const PostFormModal = () => {
   const { postFormModalVisibility, setPostFormModalVisibility } =
@@ -20,6 +22,8 @@ const PostFormModal = () => {
           <h3>Create a Post</h3>
           <Input placeholder={"Title"} />
           <TextArea placeholder={"Text (required)"} />
+
+          <ReactMarkdown remarkPlugins={[gfm]} children={""}/>
           <button className="post-form-btn">POST</button>
         </div>
       </OutsideClickHandler>
--- convertDate.js
+++ convertDate.js
@@ -1,29 +1,28 @@
-let todaysDate = new Date();
+// let todaysDate = new Date();
 
-function convertDate(date) {
-  var yyyy = date.getFullYear().toString();
-  var mm = (date.getMonth()+1).toString();
-  var dd  = date.getDate().toString();
+// function convertDate(date) {
+//   var yyyy = date.getFullYear().toString();
+//   var mm = (date.getMonth()+1).toString();
+//   var dd  = date.getDate().toString();
 
-  var mmChars = mm.split('');
-  var ddChars = dd.split('');
+//   var mmChars = mm.split('');
+//   var ddChars = dd.split('');
 
-  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
-}
-
-console.log(todaysDate)
-
-// var omar= "12/30/1997";
-
-// function formatdate(){
-//   y  = omar.getFullYear().toString();
-//   m = omar.getMonth().toString();
-//   d = omar.getDate().toString();
-//   omar=y+m+d;
-//   return omar;
+//   return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
 // }
-// console.log(formatDate("12/31/2014"));
 
+// console.log(todaysDate)
 
-let dateTime = DateTime.Parse("01/01/2001");
-console.log(dateTime)
+// // var omar= "12/30/1997";
+
+// // function formatdate(){
+// //   y  = omar.getFullYear().toString();
+// //   m = omar.getMonth().toString();
+// //   d = omar.getDate().toString();
+// //   omar=y+m+d;
+// //   return omar;
+// // }
+// // console.log(formatDate("12/31/2014"));
+
+// let dateTime = DateTime.Parse("01/01/2001");
+// console.log(dateTime)
