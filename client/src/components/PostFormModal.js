656c5181c9ef0f5a514850b6c3dac944809487f7
Blackflame6 <engineerfranklyn@gmail.com>
2023-02-20 21:55:52 -0600

    working on input boxes

--- client/src/components/PostFormModal.js
+++ client/src/components/PostFormModal.js
@@ -3,12 +3,15 @@ import { useState, useContext } from "react";
 import AuthModalContext from "../context/AuthModalContext";
 import Input from "./Input";
 import TextArea from "./TextArea";
-import ReactMarkdown from "react-markdown"
-import gfm from "remark-gfm"
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 const PostFormModal = () => {
   const { postFormModalVisibility, setPostFormModalVisibility } =
     useContext(AuthModalContext);
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
   return (
     <div
       className={
@@ -20,10 +23,11 @@ const PostFormModal = () => {
       >
         <div className="post-modal-sub">
           <h3>Create a Post</h3>
-          <Input placeholder={"Title"} />
-          <TextArea placeholder={"Text (required)"} />
-
-          <ReactMarkdown remarkPlugins={[gfm]} children={""}/>
+          <Input placeholder={"Title"} value={title} />
+          <TextArea placeholder={"Text (required)"} value={body} />
+          <div>
+            <ReactMarkdown remarkPlugins={[gfm]} children={""} />
+          </div>
           <button className="post-form-btn">POST</button>
         </div>
       </OutsideClickHandler>
