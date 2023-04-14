db2369d07d62e8a782e927de0a6aed6f7cde9f19
Blackflame6 <engineerfranklyn@gmail.com>
2023-04-13 23:17:30 -0500

    creayed voting div class

--- client/src/components/Voting.js
+++ client/src/components/Voting.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BiUpvote } from "react-icons/bi";
+import { BiDownvote } from "react-icons/bi";
 
 const Voting = () => {
   return (
-    <div className='Voting-div'>
-      V
+    <div className='voting-div'>
+      <BiUpvote className='vote-icon'/>
+      <BiDownvote className='vote-icon' />
     </div>
   )
 }
