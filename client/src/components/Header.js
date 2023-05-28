e202d96ecb7049ffcab9d1132c4124839c66cbf1
Blackflame6 <engineerfranklyn@gmail.com>
2023-05-28 22:10:58 +0100

    added create community button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,6 +8,8 @@ import { CiUser } from "react-icons/ci";
 import { BsBell } from "react-icons/bs";
 import { BsChatDots } from "react-icons/bs";
 import { HiOutlinePlus } from "react-icons/hi";
+import { BsFillFileEarmarkPostFill } from "react-icons/bs";
+import { FaUsers } from "react-icons/fa";
 import logo from "../images/logo.png";
 import { CiSearch } from "react-icons/ci";
 import { BsChevronDown } from "react-icons/bs";
@@ -20,13 +22,15 @@ import axios from "axios";
 import RedirectContext from "../context/RedirectContext";
 
 const Header = () => {
-  const [userDropDownVisibilityClass, setUserDropDownVisibilityClass] = useState("hidden");
+  const [userDropDownVisibilityClass, setUserDropDownVisibilityClass] =
+    useState("hidden");
+  const [plusDropDownVisibilityClass, setPlusDropDownVisibilityClass] =
+    useState("hidden");
   const { setModalVisibility } = useContext(AuthModalContext);
   const { setModalType } = useContext(ModalContext);
   const { user, setUser } = useContext(UserContext);
-  const { redirect, setRedirect} = useContext(RedirectContext)
-  const [searchText, setSearchText] = useState("")
-
+  const { redirect, setRedirect } = useContext(RedirectContext);
+  const [searchText, setSearchText] = useState("");
 
   const handleLogin = () => {
     setModalVisibility(true);
@@ -46,6 +50,14 @@ const Header = () => {
     }
   };
 
+  const togglePlusDropDown = () => {
+    if (plusDropDownVisibilityClass === "hidden") {
+      setPlusDropDownVisibilityClass("show");
+    } else {
+      setPlusDropDownVisibilityClass("hidden");
+    }
+  };
+
   const logout = async () => {
     await axios.get(
       // "https://redditt-api.onrender.com/logout",
@@ -59,18 +71,16 @@ const Header = () => {
   };
 
   const doSearch = (e) => {
-    e.preventDefault()
-    setRedirect("/search/" + encodeURIComponent(searchText))
-  }
+    e.preventDefault();
+    setRedirect("/search/" + encodeURIComponent(searchText));
+  };
   return (
     <header className="header">
       <div className="sub-header">
         <Link to="/">
           <img className="logo" src={logo} alt="" />
         </Link>
-        <form className="form" 
-        onSubmit={doSearch}
-        >
+        <form className="form" onSubmit={doSearch}>
           <CiSearch className="search-icon" />
 
           <input
@@ -79,22 +89,68 @@ const Header = () => {
             type="text"
             placeholder="Search Reddit"
             value={searchText}
-            onChange={(e)=> setSearchText(e.target.value)}
+            onChange={(e) => setSearchText(e.target.value)}
           />
         </form>
-        {user.username && (
-          <>
-            <button className="icon-btn">
-              <BsBell className="icon" />
-            </button>
-            <button className="icon-btn">
-              <BsChatDots className="icon" />
-            </button>
-            <button className="icon-btn">
-              <HiOutlinePlus className="icon" />
-            </button>
-          </>
-        )}
+        <OutsideClickHandler
+          onOutsideClick={() => setPlusDropDownVisibilityClass("hidden")}
+        >
+          {user.username && (
+            <>
+              <button className="icon-btn">
+                <BsBell className="icon" />
+              </button>
+              <button className="icon-btn">
+                <BsChatDots className="icon" />
+              </button>
+              <button className="icon-btn" onClick={togglePlusDropDown}>
+                <HiOutlinePlus className="icon" />
+              </button>
+
+              <div
+                className={
+                  plusDropDownVisibilityClass === "hidden"
+                    ? "hide-box"
+                    : "show-box"
+                }
+              >
+                <button className="btn link-box">
+               
+                  Create new post
+                  {/* <BsFillFileEarmarkPostFill
+                  className=" icon" /> */}
+                  </button>
+
+                <button className="btn link-box">
+                  Create new community
+                {/* <FaUsers 
+                className=" icon" /> */}
+                  </button>
+              </div>
+            </>
+
+            // <>
+            // <button className="avatar-btn" onClick={toggleDropDown}>
+            //   <img src={avatar} alt="" className="avatar" />
+            //   <BsChevronDown className="chevron" />
+            // </button>
+
+            // <div
+            //   className={
+            //     userDropDownVisibilityClass === "hidden"
+            //       ? "hide-box"
+            //       : " show-box"
+            //   }
+            // >
+            //   <p>Welcome, {user.username}</p>
+            //   <button onClick={logout} href="" className="btn link-box">
+            //     <SlLogout className=" login-icon" />
+            //     Logout
+            //   </button>
+            // </div>
+            // </>
+          )}
+        </OutsideClickHandler>
         {!user.username && (
           <div className="login-div">
             <Headerbuttons onClick={handleLogin}>Log In </Headerbuttons>
@@ -114,12 +170,6 @@ const Header = () => {
             </button>
           )}
 
-          {user.username && (
-            <button className="avatar-btn" onClick={toggleDropDown}>
-              <img src={avatar} alt="" className="avatar" />
-              <BsChevronDown className="chevron" />
-            </button>
-          )}
           {!user.username && (
             <div
               onClick={() => setModalVisibility(true)}
@@ -135,20 +185,28 @@ const Header = () => {
               </button>
             </div>
           )}
+
           {user.username && (
-            <div
-              className={
-                userDropDownVisibilityClass === "hidden"
-                  ? "hide-box"
-                  : " show-box"
-              }
-            >
-              <p>Welcome, {user.username}</p>
-              <button onClick={logout} href="" className="btn link-box">
-                <SlLogout className=" login-icon" />
-                Logout
+            <>
+              <button className="avatar-btn" onClick={toggleDropDown}>
+                <img src={avatar} alt="" className="avatar" />
+                <BsChevronDown className="chevron" />
               </button>
-            </div>
+
+              <div
+                className={
+                  userDropDownVisibilityClass === "hidden"
+                    ? "hide-box"
+                    : " show-box"
+                }
+              >
+                <p>Welcome, {user.username}</p>
+                <button onClick={logout} href="" className="btn link-box">
+                  <SlLogout className=" login-icon" />
+                  Logout
+                </button>
+              </div>
+            </>
           )}
         </OutsideClickHandler>
       </div>
