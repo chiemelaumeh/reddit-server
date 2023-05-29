1cfa0c5b0575859134801754c81bf1b404695cb8
Blackflame6 <engineerfranklyn@gmail.com>
2023-05-29 20:51:51 +0100

    fixed bug to show profile picture upon login

--- api/server.js
+++ api/server.js
@@ -11,10 +11,6 @@ import User from "./models/User.js";
 import Comment from "./models/Comments.js";
 import router from "./routes/VotingRoutes.js";
 import Vote from "./models/Votes.js";
-// import getUserFromToken from "./UserFunctions.js";
-
-
-
 
 const app = express();
 app.use(express.json());
@@ -31,14 +27,11 @@ app.use(
 const secret = process.env.SECRET_KEY;
 const connectionString = process.env.DATABASE_URL;
 
-
-app.use(router)
-
+app.use(router);
 
 const getUserFromToken = async (token) => {
   const userInfo = await jwt.verify(token, secret);
   return await User.findById(userInfo.id);
-
 };
 
 mongoose.set("strictQuery", false);
@@ -50,18 +43,20 @@ const db = mongoose.connection;
 db.on("error", console.log);
 
 app.get("/", async (req, res) => {
-  const comment = await Comment.find({});
-  res.json(comment);
+  const comment = await Comment.find({
+    rootId: { $exists: false },
+  });
+  res.status(200).json(comment);
 });
 
 app.post("/register", async (req, res) => {
   const { email, username } = req.body;
-  const findUser = await User.findOne({ username })
+  const findUser = await User.findOne({ username });
   if (findUser) {
-    res.status(422).send("Username taken. Try again.");
-    // console.log("rwe")
-  } else {
+    res.send("Username taken. Try again.");
+    console.log("Username taken. Try again.")
 
+  } else {
     const password = bcrypt.hashSync(req.body.password, 10);
     const createUser = async () => {
       const user = new User({
@@ -69,31 +64,32 @@ app.post("/register", async (req, res) => {
         username,
         password,
       });
-    try {
-      const info = await user.save();
-      res.status(201).send(`profile created for ${username}, now please Log in!`);
-  
-      jwt.sign({ id: user._id }, secret, (err, token) => {
-        if (err) {
-          console.log(err);
-          res.status(500);
-        } else {
-          // console.log(token);
-          res.status(201).cookie("token", token).send();
-        }
-      });
-    } catch (error) {
-      console.error(error.mzsasaessage);
-      res.status(500);
-    }
-  };
-  
-  createUser();
+      try {
+        const info = await user.save();
+        res
+          .status(201)
+          .send(`profile created for ${username}, now please Log in!`);
 
+        // jwt.sign({ id: user._id }, secret, (err, token) => {
+        //   if (err) {
+        //     console.log(err);
+        //     res.status(500);
+        //   } else {
+        //     // console.log(token);
+        //     res.status(201).cookie("token", token).send();
+        //   }
+        // });
+      } catch (error) {
+        console.error(error.message);
+        
+        res.status(500);
+      }
+    };
+
+    createUser();
   }
-  ;
   // if (user.username === username && user.password === password) {
-    // }
+  // }
 });
 
 app.get("/user", (req, res) => {
@@ -123,37 +119,29 @@ app.post("/login", (req, res) => {
   const findUser = async () => {
     try {
       const user = await User.findOne({ username });
-      // if (user.username === username && user.password === password) {
-      // }
       if (user && user.username == username) {
         const passOk = bcrypt.compareSync(password, user.password);
         if (passOk) {
           jwt.sign({ id: user._id }, secret, (err, token) => {
             const getUser = async () => {
-              // const token = req.cookies.token;
               try {
                 const user = await getUserFromToken(token);
-                // res.clearCookie("token", "").send();
-                // console.log(user)
                 res.cookie("token", token).json({ username: user.username });
                 console.log(token);
               } catch (err) {
-                // console.log("error45")
                 console.error(err.message);
                 res.status(500);
               }
             };
             getUser();
-            // const user = await getUserFromToken(token)
           });
         } else {
-          res.status(422).send();
+          res.status(422).send("invalid password");
           console.log("invalid password");
         }
       } else {
         res.status(422).send("Invalid username");
-
-        console.log("invalid user");
+        console.log("invalid username");
       }
     } catch (error) {
       console.log(error.message);
@@ -163,39 +151,34 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/comments", async (req, res) => {
-  // const createCommment = async () => {
-    const token = req.cookies.token
-    try {
-      const userInfo = await getUserFromToken(token);
-      const { title, body, parentId, rootId } = req.body;
-      const comment = new Comment({
-        title,
-        body,
-        author: userInfo.username,
-        postedAt: Date.now(),
-        parentId,
-        rootId
-      });
-      const newComment = await comment.save();
-      // console.log(newComment)
-      res.status(201).json(newComment);
-    } catch (error) {
-      console.error(error.message);
-    }
-  // };
-  // createCommment();
+  const token = req.cookies.token;
+  try {
+    const userInfo = await getUserFromToken(token);
+    const { title, body, parentId, rootId } = req.body;
+    const comment = new Comment({
+      title,
+      body,
+      author: userInfo.username,
+      postedAt: Date.now(),
+      parentId,
+      rootId,
+    });
+    const newComment = await comment.save();
+    res.status(201).json(newComment);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 app.get("/comments", async (req, res) => {
-  const search = req.query.search
-  const filter = search ? {title: {$regex: '.*' + search + '.*'}} : {rootId:null}
-  
-  try {
-    const comments = await Comment.find(filter).sort({postedAt: -1});
-   
+  const search = req.query.search;
+  const filter = search
+    ? { title: { $regex: ".*" + search + ".*" } }
+    : { rootId: null };
 
+  try {
+    const comments = await Comment.find(filter).sort({ postedAt: -1 });
     res.json(comments);
-    // console.log(comments)
   } catch (err) {
     console.error(err.message);
   }
@@ -206,45 +189,43 @@ app.get("/comments/:id", async (req, res) => {
     const comment = await Comment.findById(req.params.id);
     res.json(comment);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
 });
 
-
 app.get("/comments/root/:rootId", async (req, res) => {
   try {
-    const comments = await Comment.find({rootId:req.params.rootId}).sort({postedAt: -1});
+    const comments = await Comment.find({ rootId: req.params.rootId }).sort({
+      postedAt: -1,
+    });
     res.json(comments);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
 });
 
 app.get("/comments/parent/:parentId", async (req, res) => {
   try {
-    const comments = await Comment.find({parentId:req.params.parentId});
-    res.json(comments)
+    const comments = await Comment.find({ parentId: req.params.parentId });
+    res.json(comments);
   } catch (err) {
-    console.error(err.message) 
+    console.error(err.message);
   }
-})
+});
 
-async function deleteAll () {
- await Comment.deleteMany({ 
-  //  $expr: { $lt: [ { $strLenCP: "$body" }, 20 ] },
-   rootId: { $exists: true },
-
- 
-  })
+async function deleteAll() {
+  await Comment.deleteMany({
+    //  $expr: { $lt: [ { $strLenCP: "$body" }, 20 ] },
+    rootId: { $exists: true },
+  });
 
   await Vote.deleteMany({
-   direction: {$exists: true}
+    direction: { $exists: true },
+  });
 
-  })
-
-    console.log("Deleted All")
+  console.log("Deleted All");
 }
- 
+
 // deleteAll()
 app.listen(4000, () => {
   console.log("Listening on Port 4000");
