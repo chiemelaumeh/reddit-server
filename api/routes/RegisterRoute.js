3f610df4e46f0f5aae661f9357befa71c9b84c63
Blackflame6 <engineerfranklyn@gmail.com>
2023-06-12 01:51:38 -0500

    created different routes and formatted code

---
+++ api/routes/RegisterRoute.js
@@ -0,0 +1,35 @@
+import express from "express";
+const router = express.Router();
+import dotenv from "dotenv";
+dotenv.config();
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+
+router.post("/register", async (req, res) => {
+  const { email, username } = req.body;
+  const findUser = await User.exists({ username });
+  if (findUser) {
+    res.send("Username taken. Try again.");
+    console.log("Username taken. Try again.");
+  } else {
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const user = new User({
+      email,
+      username,
+      password,
+    });
+    try {
+      const info = await user.save();
+      res
+        .status(201)
+        .send(`profile created for ${username}, now please Log in!`);
+      console.log(`profile created for ${username}, now please Log in!`);
+    } catch (error) {
+      console.error(error.message);
+
+      res.status(500);
+    }
+  }
+
+});
+export default router
--- api/server.js
+++ api/server.js
@@ -7,17 +7,26 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+
 import User from "./models/User.js";
-import Comment from "./models/Comments.js";
-import VotingRoutes from "./routes/VotingRoutes.js";
-import CommunityRoutes from "./routes/CommunityRoute.js";
 import Community from "./models/Community.js";
 import Vote from "./models/Votes.js";
+import Comment from "./models/Comments.js";
+
+
+import RegisterRoute from "./routes/RegisterRoute.js"
+import VotingRoutes from "./routes/VotingRoutes.js";
+import CommunityRoutes from "./routes/CommunityRoute.js";
+import UserRoute from "./routes/UserRoute.js"
+import LogoutRoute from "./routes/LogoutRoute.js"
+import PostCommentRoute from "./routes/PostCommentRoute.js"
+import GetCommentsRoute from "./routes/GetCommentsRoute.js"
+import DeleteCommentRoute from "./routes/DeleteCommentRoute.js"
+
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://reddit-app-nw97.onrender.com"],
@@ -25,12 +34,23 @@ app.use(
     credentials: true,
   })
 );
-
-const secret = process.env.SECRET_KEY;
-const connectionString = process.env.DATABASE_URL;
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(VotingRoutes);
 app.use(CommunityRoutes);
+app.use(RegisterRoute)
+app.use(UserRoute)
+app.use(LogoutRoute)
+app.use(PostCommentRoute)
+app.use(GetCommentsRoute)
+app.use(DeleteCommentRoute)
+
+
+
+
+const connectionString = process.env.DATABASE_URL;
+const secret = process.env.SECRET_KEY;
+
 
 export const getUserFromToken = async (token) => {
   const userInfo = await jwt.verify(token, secret);
@@ -52,75 +72,12 @@ app.get("/", async (req, res) => {
   res.status(200).json(comment);
 });
 
-app.post("/register", async (req, res) => {
-  const { email, username } = req.body;
-  const findUser = await User.exists({ username });
-  if (findUser) {
-    res.send("Username taken. Try again.");
-    console.log("Username taken. Try again.");
-  } else {
-    const password = bcrypt.hashSync(req.body.password, 10);
-    const user = new User({
-      email,
-      username,
-      password,
-    });
-    try {
-      const info = await user.save();
-      res
-        .status(201)
-        .send(`profile created for ${username}, now please Log in!`);
-      console.log(`profile created for ${username}, now please Log in!`);
-
-      // jwt.sign({ id: user._id }, secret, (err, token) => {
-      //   if (err) {
-      //     console.log(err);
-      //     res.status(500);
-      //   } else {
-      //     // console.log(token);
-      //     res.status(201).cookie("token", token).send();
-      //   }
-      // });
-    } catch (error) {
-      console.error(error.message);
-
-      res.status(500);
-    }
-  }
-  // if (user.username === username && user.password === password) {
-  // }
-});
-
-app.get("/user", (req, res) => {
-  const getUser = async () => {
-    const token = req.cookies.token;
-    try {
-      const user = await getUserFromToken(token);
-      // res.clearCookie("token", "").send();
-
-      res.json({ username: user.username });
-    } catch (err) {
-      // console.log("error45")
-      console.error(err.message);
-      res.status(500);
-    }
-  };
-
-  getUser();
-});
-
-app.get("/logout", (req, res) => {
-  res.clearCookie("token", "").send("Successfully logged out");
-  console.log("Successfully logged out");
-});
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
   const findUser = async () => {
     try {
       const user = await User.findOne({ username });
-      // console.log(user);
-      // res.json(user)
       if (user && user.username === username) {
         const passOk = bcrypt.compareSync(password, user.password);
         if (passOk) {
@@ -148,109 +105,11 @@ app.post("/login", (req, res) => {
   findUser();
 });
 
-app.post("/comments", async (req, res) => {
-  const token = req.cookies.token;
-
-  try {
-    const userInfo = await getUserFromToken(token);
-    const { title, body, parentId, rootId } = req.body;
-    const chosenCommunity = req.body.selectedCommunity || req.body.chosenCommunity
-    console.log(req.body);
-    const communityExists = await Community.findOne({ name: chosenCommunity });
-    // console.log(communityExists)
-    // NEEDS MORE WORK
-    // NEEDS MORE WORK
-    // NEEDS MORE WORK
-    // NEEDS MORE WORK
-    
-    const comment = new Comment({
-      title,
-      body,
-      chosenCommunity,
-      author: userInfo.username,
-      postedAt: Date.now(),
-      parentId,
-      rootId,
-    });
-    const newComment = await comment.save();
-    res.status(201).json(newComment);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-app.get("/comments", async (req, res) => {
-  const { search, chosenCommunity } = req.query;
-  const filter = search
-    ? { title: { $regex: ".*" + search + ".*" } }
-    : { rootId: null };
-
-  if (chosenCommunity) {
-    filter.chosenCommunity = chosenCommunity;
-  }
-
-  try {
-    const comments = await Comment.find(filter).sort({ postedAt: -1 });
-    res.json(comments);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.get("/comments/:id", async (req, res) => {
-  try {
-    const comment = await Comment.findById(req.params.id);
-    res.json(comment);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.get("/comments/root/:rootId", async (req, res) => {
-  try {
-    const comments = await Comment.find({ rootId: req.params.rootId }).sort({
-      postedAt: -1,
-    });
-    res.json(comments);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.get("/comments/parent/:parentId", async (req, res) => {
-  try {
-    const comments = await Comment.find({ parentId: req.params.parentId });
-    res.json(comments);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-async function myFinder() {
-  const showCommunities = await Comment.find({
-    community: { $exists: true },
-  });
-  console.log(showCommunities);
-}
-
-// myFinder()
-
-app.delete("/delete/:id", async(req, res) => {
-  const { id } = req.params
-  // console.log(id)
-  try {
-    const commentToDelete = await Comment.deleteOne({_id:id})
-    res.json(id)
 
 
-    
-  } catch (error) {
-    console.error(error.message)
-  }
-})
 async function deleteAll() {
   await Comment.deleteMany({
-    // body: { $exists: true },
+
     $expr: { $lt: [{ $strLenCP: "$body" }, 20] },
   });
 
@@ -258,7 +117,7 @@ async function deleteAll() {
       direction: { $exists: true },
     });
    await Community.deleteMany({
-    // name: { $exists: true}
+
         $expr: { $lt: [ { $strLenCP: "$avatar" }, 10 ] },
    })
   console.log("Deleted All");
