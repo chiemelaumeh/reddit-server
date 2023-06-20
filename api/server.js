b25671e78d0fa435f240794e17aa512792d1cb4d
Blackflame6 <engineerfranklyn@gmail.com>
2023-06-20 03:50:58 -0500

    try

--- api/server.js
+++ api/server.js
@@ -28,14 +28,14 @@ import LoginRoute from "./routes/LoginRoute.js";
 import { connectDb } from "./config/db.js";
 
 
-const app = express();
+const app = express(); 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: [
       // "http://localhost:3000",
-      "https://myreddit-megq.onrender.com"
+      // "https://myreddit-megq.onrender.com"
       ],
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
