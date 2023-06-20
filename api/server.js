647b008027d4ec0c9da93e6404a5ea930d40edb1
Blackflame6 <engineerfranklyn@gmail.com>
2023-06-20 02:21:40 -0500

    ready to deploy backend

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 import path from "path";
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from "url";
 
 import User from "./models/User.js";
 import Community from "./models/Community.js";
@@ -23,29 +23,30 @@ import GetCommentsRoute from "./routes/GetCommentsRoute.js";
 import DeleteCommentRoute from "./routes/DeleteCommentRoute.js";
 import UploadRRoute from "./routes/UploadRoute.js";
 import ImageRoute from "./routes/ImageRoute.js";
-import LoginRoute from "./routes/LoginRoute.js"
+import LoginRoute from "./routes/LoginRoute.js";
 
 import { connectDb } from "./config/db.js";
+
+
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://reddit-app-nw97.onrender.com"],
+    origin: [
+      // "http://localhost:3000",
+      "https://myreddit-megq.onrender.com"
+      ],
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   })
-  
 );
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.get("/", async (req, res) => {
-  const comment = await Comment.find({
-    rootId: { $exists: false },
-  });
-  res.status(200).json(comment);
-});
+app.set('trust proxy', 1);
 app.use("/votes", VotingRoutes);
 app.use("/communities", CommunityRoutes);
 app.use("/register", RegisterRoute);
@@ -56,69 +57,36 @@ app.use("/comments", GetCommentsRoute);
 app.use("/delete", DeleteCommentRoute);
 app.use("/upload", UploadRRoute);
 app.use("/image", ImageRoute);
-app.use("/login", LoginRoute)
+app.use("/login", LoginRoute);
 
 
 
-// const __filename = fileURLToPath(import.meta.url);
-// console.log(__filename)
-
-// 👇️ "/home/borislav/Desktop/javascript"
-
-// console.log('directory-name 👉️', __dirname);
-// 👇️ "/home/borislav/Desktop/javascript/dist/index.html"
-// console.log(path.join(__dirname, '/dist', 'index.html'))
-
-
-// const __dirname = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-// app.use(express.static(path.join(__dirname, '../client/build')))
-// app.get('*', (req, res)=> {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"))
-
-// })
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("myReddit API is running")
-//   })
-// }
 
 connectDb();
 
-
-
-
-const secret = process.env.SECRET_KEY
+const secret = process.env.SECRET_KEY;
 export const getUserFromToken = async (token) => {
   const userInfo = await jwt.verify(token, secret);
   return await User.findById(userInfo.id);
 };
 
 
-app.get("/", async (req, res) => {
-  const comment = await Comment.find({
-    rootId: { $exists: false },
-  });
-  res.status(200).json(comment);
-});
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.resolve();
 
-
-// async function deleteAll() {
-//   await Comment.deleteMany({
-//     $expr: { $lt: [{ $strLenCP: "$body" }, 20] },
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 //   });
-
-//   await Vote.deleteMany({
-//     direction: { $exists: true },
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("myReddit API is running");
 //   });
-//   await Community.deleteMany({
-//     $expr: { $lt: [{ $strLenCP: "$avatar" }, 10] },
-//   });
-//   console.log("Deleted All");
 // }
-// 
-// deleteAll();
+
+
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
