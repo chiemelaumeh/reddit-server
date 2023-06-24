5ebae1833e6d25b802d515e8ba3f69eb17b07618
Blackflame6 <engineerfranklyn@gmail.com>
2023-06-24 17:56:30 -0500

    overhaul9a

--- api/server.js
+++ api/server.js
@@ -52,7 +52,7 @@ app.use(
       // "http://10.0.0.189:3000",
       // "http://franklyn.local:3000",
       "https://myreddit-megq.onrender.com",
-      "https://myreddit-api.onrender.com"
+      // "https://myreddit-api.onrender.com"
         //  "http://localhost:4000"
     ],
     methods: ["GET", "POST", "DELETE"],
