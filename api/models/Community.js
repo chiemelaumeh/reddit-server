0777a79ee6949de12ecbe2123631d0a2dad0080e
Blackflame6 <engineerfranklyn@gmail.com>
2023-07-04 16:17:52 -0500

    overhaul15

--- api/models/Community.js
+++ api/models/Community.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose"
 const communitySchema = new mongoose.Schema({
   name : {type: String, required: true},
   slogan : {type: String, required: true},
-  avatar : {type: String, required: false},
-  cover: {type: String, required: false},
+
 })
 
 const Community = new mongoose.model("Community", communitySchema)
