5b0549c04a3d70d6dc0461f2c5f931cb6032261c
Blackflame6 <engineerfranklyn@gmail.com>
2023-07-05 15:21:14 -0500

    build16 a

--- client/src/context/ModalContext.js
+++ client/src/context/ModalContext.js
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 const ModalContext = createContext();
 export const ModalProvider = ({ children }) => {
   const [modalType, setModalType] = useState("login");
-  const [forgotStage, setForgotStage] = useState("otp");
+  const [forgotStage, setForgotStage] = useState("");
   const [otp, setOtp] = useState(null)
   const [userEmail, setUserEmail] = useState("")
 
