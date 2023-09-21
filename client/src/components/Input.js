fc9ab73ae82bf65e4a04a7fbb7eabdc9d5f181e5
Chiemela Umeh <chiemelaumeh1@gmail.com>
2023-09-21 01:22:14 -0500

    Delete client directory

--- client/src/components/Input.js
+++
@@ -1,5 +0,0 @@
-const Input = (props) => {
-  return <input required {...props} className="input input-box" />;
-};
-
-export default Input;
--- client/src/components/TextArea.js
+++
@@ -1,5 +0,0 @@
-const TextArea = (props) => {
-  return <textarea required {...props} className="height input textarea-box"  />;
-};
-
-export default TextArea;
