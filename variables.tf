c5fb2a2d061a0d1823d00709ea7c7be359244b3c
Chiemela Umeh <chiemelaumeh1@gmail.com>
2023-10-09 12:09:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" {
     description = "Tag Name of for Ec2 instance"
-    default     = "terraform-instance"
+    default     = "BuildAgent2"
 }
 variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance"
