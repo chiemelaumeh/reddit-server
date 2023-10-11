d9e0442947ee096974d5d7d4e3baa472d5f92389
Chiemela Umeh <chiemelaumeh1@gmail.com>
2023-10-11 14:25:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" {
     description = "Tag Name of for Ec2 instance"
-    default     = "BuildAgent2"
+    default     = "BuildAgent3"
 }
 variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance"
